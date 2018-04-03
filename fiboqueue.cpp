01a753e5e41f9ecd481e3e51312d393ca4bde865
Antoine <antoine.sb@orange.fr>
2018-04-03 13:44:20 +0100

    Optimization

--- fiboqueue.cpp
+++ fiboqueue.cpp
@@ -7,41 +7,32 @@ using namespace std;
 // destructor
 	template<class T> FibQueue<T>::~FibQueue() {}
 // accessors
-	template<class T> typename unordered_map<T, typename FibHeap<T>::FibNode*>::iterator FibQueue<T>::find(T k) {
-		typename unordered_map<T, typename FibHeap<T>::FibNode*>::iterator mit = fstore.find(k);
-		return mit;
-	}
-	template<class T> typename FibHeap<T>::FibNode* FibQueue<T>::findNode(T k) {
-		typename unordered_map<T, typename FibHeap<T>::FibNode*>::iterator mit = find(k);
-		return mit->second;
-	}
+	template<class T> typename unordered_map<T, typename FibHeap<T>::FibNode*>::iterator FibQueue<T>::find(T k) { return fstore.find(k); }
+	template<class T> typename FibHeap<T>::FibNode* FibQueue<T>::findNode(T k) { return find(k)->second; }
 // modifiers
 	template<class T> void  FibQueue<T>::decrease_key(typename FibHeap<T>::FibNode* x, int k) {
-		typename unordered_map<T, typename FibHeap<T>::FibNode*>::iterator mit = find(x->key);
-		fstore.erase(mit);
-		fstore.insert(pair<T, typename FibHeap<T>::FibNode*>(k, x));
+		fstore.erase(find(x->key));
+		fstore.emplace(k, x);
 		FibHeap<T>::decrease_key(x, k);
 	}
 	template<class T> void  FibQueue<T>::pop() {
-		if (FibHeap<T>::empty())
+		if(FibHeap<T>::empty())
 			return;
 		typename FibHeap<T>::FibNode* x = FibHeap<T>::extract_min();
-		if(!x)
-			return; // should not happen.
-		auto range = fstore.equal_range(x->key);
-		auto mit = find_if(range.first, range.second,
-			[x](const pair<T, typename FibHeap<T>::FibNode*> &ele) {
+		auto range = fstore.equal_range(x->key), mit = find_if(range.first, range.second,
+			[x](const auto& ele) {
 				return ele.second == x;
 			}
 		);
 		if(mit != range.second)
 			fstore.erase(mit);
-		else cerr << "[Error]: key " << x->key << " cannot be found in FiboQueue fast store" << endl;
+		else
+			cerr << "[Error]: key " << x->key << " cannot be found in FiboQueue fast store" << endl;
 		delete x;
 	}
 	template<class T> typename FibHeap<T>::FibNode* FibQueue<T>::push(T k, void* pl) {
-		typename FibHeap<T>::FibNode* x = FibHeap<T>::push(k, pl);
-		fstore.insert(pair<T, typename FibHeap<T>::FibNode*>(k, x));
+		auto x = FibHeap<T>::push(k, pl);
+		fstore.emplace(k, x);
 		return x;
 	}
 	template<class T> typename FibHeap<T>::FibNode* FibQueue<T>::push(T k) { return push(k, NULL); }
